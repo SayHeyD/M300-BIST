8be2b39bb5ef80c9749ec9a7775159f4ccbd4a94
David <david@docampo.ch>
2020-09-27 21:08:12 +0200

    fixing stuff

--- docker-files/phone-book/phone-book-app/setup.sh
+++ docker-files/phone-book/phone-book-app/setup.sh
@@ -3,5 +3,9 @@
 # Start nginx Service
 service nginx restart
 
+# Setup laravel project
+php /var/www/phone-book-app/artisan key:generate
+php /var/www/phone-book-app/artisan key:migrate
+
 #Start php-fpm Server and keep it running
 php-fpm
